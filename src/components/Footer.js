e29bb052d115cc37a508cc35263a097ec70b0f34
Ernest Isaac <ieifeanyichukwu@gmail.com>
2021-11-25 15:03:19 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,7 +44,7 @@ export const Footer = () => {
             </div>
             <div className="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
-                Coded by <a href="https://ernestizic.netlify.app">ernestizic</a>.
+                Coded by <a href="https://ernestizic.tech">Ernestizic</a>.
             </div>
 
             
